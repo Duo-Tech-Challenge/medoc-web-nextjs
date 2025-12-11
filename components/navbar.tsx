5507165f5943203d537a89e54925539bad9db987
ibraum <ibraumodnok@gmail.com>
2025-12-11 13:37:43 +0000

    docker compose

---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='flex justify-between items-center'>
+      <div className="w-2xl"></div>
+      <div className="flex items-center justify-center gap-4"></div>
+      <div className="w-2xl"></div>
+    </div>
+  )
+}
+
+export default navbar
